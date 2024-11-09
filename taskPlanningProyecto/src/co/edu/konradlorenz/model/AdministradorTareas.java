f1807c5e067bc34c16cf7c1877bab99d896092b1
Camilo Prieto <prietocamilo21@gmail.com>
2024-11-08 22:39:52 -0500

    NEW >>>> Se agrego clases al model
    Se agregaron las clases para "Tarea" que almacena información de cada tarea y la clase "AdministradorTareas" para crear una Cola con ArrayList.

---
+++ taskPlanningProyecto/src/co/edu/konradlorenz/model/AdministradorTareas.java
@@ -0,0 +1,31 @@
+package co.edu.konradlorenz.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AdministradorTareas {
+
+    //Atributos
+    private ArrayList<Tarea> colaArrayList;
+
+    //Constructor
+    public AdministradorTareas() {
+        this.colaArrayList = new ArrayList<>();
+    }//Cierre constructor
+
+    public ArrayList<Tarea> getColaArrayList() {
+        return colaArrayList;
+    }
+
+    public void agregarTarea(Tarea tarea) {
+        colaArrayList.add(tarea);
+    }//Cierre agregar tarea
+
+    public boolean estaVacia() {
+        return colaArrayList.isEmpty();
+    }
+
+}//Cierre AdministradirTareas
---
+++ taskPlanningProyecto/src/co/edu/konradlorenz/model/Tarea.java
@@ -0,0 +1,66 @@
+package co.edu.konradlorenz.model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Tarea {
+
+    //Atributos de la tarea
+    private String nombreTarea;
+    private String descripcion;
+    private int prioridad;
+
+    //Constructor
+    public Tarea(String nombreTarea, String descripcion, int prioridad) {
+        this.nombreTarea = nombreTarea;
+        this.descripcion = descripcion;
+        this.prioridad = prioridad;
+    }//Cierre Constructor
+
+    //Constructor vacio
+    public Tarea() {
+    }
+
+    //Getters & Setters
+    public String getNombreTarea() {
+        return nombreTarea;
+    }
+
+    public void setNombreTarea(String nombreTarea) {
+        this.nombreTarea = nombreTarea;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(int prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    //ToString
+    @Override
+    public String toString() {
+        return getActualTime()+ " "+ prioridad + ". " + nombreTarea + " | Descripción: " + descripcion;
+    }
+
+    //Metodo para mostrar fecha de manera agradable a la vista
+    public String getActualTime() {
+        LocalDateTime fechaHoraActual = LocalDateTime.now(); //Formato de LocalDate Time: YYYY-MM-DDTHH:MM:SS
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm:ss dd/MM/yyyy"); //Formato más agradable a la vista.
+        String actualTime = fechaHoraActual.format(formato);
+        return actualTime;
+    }//Cierre getActualTime
+}//Cierre tarea
