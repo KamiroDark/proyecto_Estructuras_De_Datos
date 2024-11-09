0b2bce419d4604eb10bab91481458eab58100991
Camilo Prieto <prietocamilo21@gmail.com>
2024-11-08 22:40:51 -0500

    IN DEV >>>> Se agrego la funcionalidad de agregar tarea
    Por el momento se agrego la funcionalidad de agregar tarea a la lista y el menú de inicio cuando se ingres al programa.

--- taskPlanningProyecto/src/co/edu/konradlorenz/controller/Control.java
+++ taskPlanningProyecto/src/co/edu/konradlorenz/controller/Control.java
@@ -1,16 +1,54 @@
 
 package co.edu.konradlorenz.controller;
 
+import co.edu.konradlorenz.model.AdministradorTareas;
+import co.edu.konradlorenz.model.Tarea;
 import co.edu.konradlorenz.view.Vista;
 
 
 public class Control {
     
     protected Vista objVista = new Vista();
+    protected AdministradorTareas listaTareas = new AdministradorTareas();
     
     //Metodo para correr el programa
     public void run(){
+        objVista.mostrarMensaje("TasKonrad Planning");
+        int opcion;
+        do {
+        objVista.mensajito();
+        mostrarTareas();
         objVista.menu();
+        
+        opcion = objVista.pedirInt("Digita una opcion del menu!:");
+            switch (opcion) {
+                case 1:
+                    String nombre = objVista.pedirString("Ingresa el nombre de la tarea:");
+                    String descripcion = objVista.pedirString("Agrega una descripción para acordarte ;)");
+                    int prio = objVista.pedirInt("Que prioridad le quieres dar?(1, 2, 3...)");
+                    Tarea nuevaTarea = new Tarea(nombre, descripcion, prio);
+                    listaTareas.agregarTarea(nuevaTarea);
+                    break;
+                case 0:
+                    objVista.mostrarMensaje("Nos vemos al rato!, Saliendo...");
+                    System.exit(0);
+                    break;
+                default:
+                    objVista.mostrarMensaje("Opción incorrecta, selecciona un numero dentro del menú");
+            }
+        } while (true);
     }//Cierre Run
     
+    //Metodo para mostrar tareas
+    public void mostrarTareas() {
+        boolean vacio = listaTareas.estaVacia();
+
+        if (vacio) {
+            objVista.mostrarMensaje("No hay tareas en la lista");
+        } else {
+            for (Tarea tarea : listaTareas.getColaArrayList()) {
+                objVista.estadoTarea("-", tarea);
+            }
+        }
+        }//Cierre mostrarTareas
 }//Cierre Control
--- taskPlanningProyecto/src/co/edu/konradlorenz/view/Vista.java
+++ taskPlanningProyecto/src/co/edu/konradlorenz/view/Vista.java
@@ -1,6 +1,6 @@
-
 package co.edu.konradlorenz.view;
 
+import co.edu.konradlorenz.model.Tarea;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,17 +9,16 @@ import java.util.Scanner;
  * @author Usuario
  */
 public class Vista {
-    
+
     //Scanner
     public static Scanner sc = new Scanner(System.in);
-    
-    //Metodo para menú
-    public void menu(){
-        System.out.println("------Task Planning------");
-        
+
+    //Metodo para mostrar mensajito aleatorio al usuario
+    public void mensajito() {
+
         //Mensajito aleatorio para el usuario :3
         Random random = new Random();
-	int numeroAleatorio = random.nextInt(5) + 1;
+        int numeroAleatorio = random.nextInt(5) + 1;//Se generan de 1 a 5 numeros aleatorios
         switch (numeroAleatorio) {
             case 1:
                 System.out.println("Recuerda tomar agua!");
@@ -35,11 +34,44 @@ public class Vista {
                 break;
             case 5:
                 System.out.println("Bienvenid@ de vuelta!!!");
+                break;
             default:
                 System.out.println("Bienvenid@ a Task Planning");//Por si acaso no salta el mensaje :D
         }//Cierre switch
-        
-        
+
+    }//Cierre Mensajito
+
+    //Metodo para el menu de opciones
+    public void menu() {
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>");
+        System.out.println("Que deseas hacer?");
+        System.out.println("1. Agregar tarea nueva");
+        System.out.println("0. Salir");
     }//Cierre menu
-    
+
+    //Metodo para mostrar un mensaje de acción y la tarea que se necesita notificar
+    public void estadoTarea(String mensaje, Tarea tarea) {
+        System.out.println(mensaje + "" + tarea);
+    }//Cierre estadoTarea
+
+    //Metodo para mostrar mensajes al usuario
+    public void mostrarMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }//Cierre mostrarMensaje
+
+    //Metodo para tomar Entero por consola
+    public int pedirInt(String mensaje) {
+        System.out.println(mensaje);
+        int op = sc.nextInt();
+        sc.nextLine();
+        return op;
+    }//Cierre pedirInt
+
+    //Metodo para pedir String
+    public String pedirString(String mensaje) {
+        System.out.println(mensaje);
+        String op = sc.nextLine();
+        return op;
+    }//Cierre pedirString
+
 }//Cierre Vista
