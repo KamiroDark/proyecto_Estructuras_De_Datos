f6b13523c8642de36174847c105d4f8152d759b8
Josel._.Patiño <168464177+J0ZB1Lgame@users.noreply.github.com>
2024-11-20 00:09:39 -0500

    Update AdministradorTareas.java

--- taskPlanningProyecto/src/co/edu/konradlorenz/model/AdministradorTareas.java
+++ taskPlanningProyecto/src/co/edu/konradlorenz/model/AdministradorTareas.java
@@ -5,27 +5,47 @@ import java.util.ArrayList;
 /*
  * @author Bautista_López_Patiño_Prieto
  */
-
-public class AdministradorTareas {
-
-    //Atributos
+ 
+ public class AdministradorTareas {
+ 
     private ArrayList<Tarea> colaArrayList;
-
-    //Constructor
+ 
     public AdministradorTareas() {
         this.colaArrayList = new ArrayList<>();
-    }//Cierre constructor
-
+    }
+ 
     public ArrayList<Tarea> getColaArrayList() {
         return colaArrayList;
     }
-
+ 
     public void agregarTarea(Tarea tarea) {
         colaArrayList.add(tarea);
-    }//Cierre agregar tarea
-
+    }
+ 
     public boolean estaVacia() {
         return colaArrayList.isEmpty();
     }
-
-}//Cierre AdministradirTareas
+ 
+    public boolean eliminarTarea(String nombreTarea) {
+        return colaArrayList.removeIf(tarea -> tarea.getNombreTarea().equalsIgnoreCase(nombreTarea));
+    }
+ 
+    public Tarea buscarTareaPorNombre(String nombreTarea) {
+        for (Tarea tarea : colaArrayList) {
+            if (tarea.getNombreTarea().equalsIgnoreCase(nombreTarea)) {
+                return tarea;
+            }
+        }
+        return null;
+    }
+ 
+    public ArrayList<Tarea> buscarTareasPorPrioridad(int prioridad) {
+        ArrayList<Tarea> tareasPorPrioridad = new ArrayList<>();
+         for (Tarea tarea : colaArrayList) {
+            if (tarea.getPrioridad() == prioridad) {
+                tareasPorPrioridad.add(tarea);
+            }
+        }
+        return tareasPorPrioridad;
+    }
+} 
