f54b3a64a18ede53540cd8928799c38cd8b1843a
Camilo Prieto <prietocamilo21@gmail.com>
2024-11-21 06:30:43 -0500

    IN DEV >>>> Comentarios y organización
    Se agregaron comentarios y organización a la clase Tarea.

--- taskPlanningProyecto/src/co/edu/konradlorenz/model/Tarea.java
+++ taskPlanningProyecto/src/co/edu/konradlorenz/model/Tarea.java
@@ -1,60 +1,70 @@
 package co.edu.konradlorenz.model;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+
 
 /*
  * @author Bautista_López_Patiño_Prieto
  */
- 
- public class Tarea {
- 
+public class Tarea implements Serializable {
+
+    private static final long serialVersionUID = 1L; // Versión de serialización
+    // -- // Atributos // -- //
     private String nombreTarea;
     private String descripcion;
     private int prioridad;
- 
+    private LocalDateTime fechaCreacion;
+
+    // -- // Constructores // -- //
     public Tarea(String nombreTarea, String descripcion, int prioridad) {
         this.nombreTarea = nombreTarea;
         this.descripcion = descripcion;
         this.prioridad = prioridad;
+        this.fechaCreacion = LocalDateTime.now();
     }
- 
+
     public Tarea() {
     }
- 
+
+    // -- // Getters & Setters // -- //
     public String getNombreTarea() {
         return nombreTarea;
     }
- 
+
     public void setNombreTarea(String nombreTarea) {
         this.nombreTarea = nombreTarea;
     }
- 
+
     public String getDescripcion() {
         return descripcion;
     }
- 
+
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
- 
+
     public int getPrioridad() {
         return prioridad;
     }
- 
+
     public void setPrioridad(int prioridad) {
         this.prioridad = prioridad;
     }
- 
+
+    // -- // ToString // -- //
     @Override
+
     public String toString() {
-        return getActualTime() + " " + prioridad + ". " + nombreTarea + " | Descripción: " + descripcion;
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm:ss dd/MM/yyyy");
+        return fechaCreacion.format(formato) + " " + prioridad + ". " + nombreTarea + " | Descripción: " + descripcion;
     }
- 
+
+    /*/ -- // Metodo para la hora y fecha en formato legible // -- //
     public String getActualTime() {
         LocalDateTime fechaHoraActual = LocalDateTime.now();
         DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm:ss dd/MM/yyyy");
         return fechaHoraActual.format(formato);
-    }
-}
- 
+    }//Cierre getActualTime/*/
+}//Cierre Tarea Class
